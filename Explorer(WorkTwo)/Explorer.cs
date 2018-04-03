de579bd5814119374613c948be12e26d45a2a7ef
mitGmailCom <shutkovdmitrij@gmail.com>
2018-04-03 20:13:10 +0300

    Create Project Explorer

---
+++ Explorer(WorkTwo)/Explorer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer_WorkTwo_
+{
+    public partial class Explorer : Form
+    {
+        public Explorer()
+        {
+            InitializeComponent();
+        }
+    }
+}
