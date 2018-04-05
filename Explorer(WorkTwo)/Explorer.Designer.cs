6b16aa66230e72b01f371688b999f9dee22d782d
mitGmailCom <shutkovdmitrij@gmail.com>
2018-04-05 11:07:33 +0300

    order Explorer

--- Explorer(WorkTwo)/Explorer.Designer.cs
+++ Explorer(WorkTwo)/Explorer.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Main = new System.Windows.Forms.TabPage();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.View = new System.Windows.Forms.TabPage();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.listbDisk = new System.Windows.Forms.ListBox();
+            this.listbView = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.Main.SuspendLayout();
+            this.View.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Main);
+            this.tabControl1.Controls.Add(this.View);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(552, 57);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.Main.Controls.Add(this.toolStrip1);
+            this.Main.Location = new System.Drawing.Point(4, 22);
+            this.Main.Name = "Main";
+            this.Main.Padding = new System.Windows.Forms.Padding(3);
+            this.Main.Size = new System.Drawing.Size(544, 31);
+            this.Main.TabIndex = 0;
+            this.Main.Text = "Главная";
+            this.Main.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(538, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // View
+            // 
+            this.View.Controls.Add(this.toolStrip2);
+            this.View.Location = new System.Drawing.Point(4, 22);
+            this.View.Name = "View";
+            this.View.Padding = new System.Windows.Forms.Padding(3);
+            this.View.Size = new System.Drawing.Size(484, 31);
+            this.View.TabIndex = 1;
+            this.View.Text = "Вид";
+            this.View.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(478, 25);
+            this.toolStrip2.TabIndex = 0;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // listbDisk
+            // 
+            this.listbDisk.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listbDisk.FormattingEnabled = true;
+            this.listbDisk.Location = new System.Drawing.Point(0, 83);
+            this.listbDisk.Name = "listbDisk";
+            this.listbDisk.Size = new System.Drawing.Size(215, 264);
+            this.listbDisk.TabIndex = 1;
+            this.listbDisk.Click += new System.EventHandler(this.listbDisk_Click);
+            this.listbDisk.DoubleClick += new System.EventHandler(this.listbDisk_DoubleClick);
+            // 
+            // listbView
+            // 
+            this.listbView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listbView.FormattingEnabled = true;
+            this.listbView.Location = new System.Drawing.Point(217, 83);
+            this.listbView.Name = "listbView";
+            this.listbView.Size = new System.Drawing.Size(335, 264);
+            this.listbView.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 21);
+            this.textBox1.TabIndex = 3;
             // 
             // Explorer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(552, 350);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listbView);
+            this.Controls.Add(this.listbDisk);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Explorer";
             this.Text = "Проводник";
+            this.Load += new System.EventHandler(this.Explorer_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Main.ResumeLayout(false);
+            this.Main.PerformLayout();
+            this.View.ResumeLayout(false);
+            this.View.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Main;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TabPage View;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ListBox listbDisk;
+        private System.Windows.Forms.ListBox listbView;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Explorer(WorkTwo)/Explorer.cs
+++ Explorer(WorkTwo)/Explorer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,190 @@ namespace Explorer_WorkTwo_
 {
     public partial class Explorer : Form
     {
+        ToolStrip OpenDrive = new ToolStrip();
+        private string NumberDrive { get; set; } = "drive"; //
+        private int Ndrive { get; set; } = 1; // 
+        List<DriveInfo> driveInfo = new List<DriveInfo>(); // список дисков
+        List<string> ListFolderView = new List<string>(); // список для отображения в листбокс View
+        private int TimeI { get; set; } = 0;//
+        private bool Flagtimer { get; set; } = false; // флаг запущен таймер
+        private string SelectIt { get; set; } = null; // выделеная строка
+        private bool AddDiskToListBoxView { get; set; } = false; // 
+
         public Explorer()
         {
             InitializeComponent();
+        }
+
+        private void Explorer_Load(object sender, EventArgs e)
+        {
+            foreach (DriveInfo item in DriveInfo.GetDrives())
+            {
+                if (item.IsReady == true)
+                {
+                    listbDisk.Items.Add(item);
+                    driveInfo.Add(item);
+                    ListFolderView.Add(item.ToString());
+                }
+            }
+            showItemsListBox();
+        }
+
+        #region ListBox-listDisk
+
+        // вывод в листБокс
+        private void showItemsListBox()
+        {
+            for (int i = 0; i < listbDisk.Items.Count; i++)
+            {
+                listbDisk.Text = listbDisk.Items[i].ToString();
+                textBox1.Text = listbDisk.Items[i].ToString();
+            }
+        }
+
+
+
+        // добавление в Items
+        private void ShowFolder(string path, int ind)
+        {
+            textBox1.Text = path;
+            int n = 1;
+            foreach (string dirs in Directory.GetDirectories(path))
+            {
+                if (dirs.Contains(path))
+                {
+                    listbDisk.Items.Insert((ind + n), "   " + dirs);
+                    ListFolderView.Insert((ind + n), "   " + dirs);
+                }
+            }
+        }
+
+        #endregion
+
+
+        private void ContinueClick(object sender, EventArgs e)
+        {
+            if (Flagtimer == true & TimeI <= 10)
+            {
+                listbDisk_DoubleClick(sender, e);
+            }
+
+            listbDisk.Text = "";
+            ShowFolder(listbDisk.SelectedItem.ToString(), listbDisk.SelectedIndex);
+            showItemsListBox();
+
+            if(SelectIt == listbDisk.SelectedItem.ToString())
+                Flagtimer = true;
+
+            SelectIt = listbDisk.SelectedItem.ToString();
+        }
+
+                
+
+
+        private void listbDisk_Click(object sender, EventArgs e)
+        {
+            if (listbDisk.SelectedItem != null & listbDisk.SelectedItem.ToString() != SelectIt)
+            {
+                if (Flagtimer == false)
+                {
+                    this.timer1.Enabled = true;
+                    this.timer1.Interval = 50;
+                    TimeI++;
+                    this.timer1.Start();
+                }
+            }
+
+            if (SelectIt == listbDisk.SelectedItem.ToString())
+            {
+                Flagtimer = true;
+                ContinueClick(sender, e);
+            }
+
+                SelectIt = listbDisk.SelectedItem.ToString();
+        }
+
+
+
+        #region Lisbox-listView
+
+        // добавление файлов в Items(listbView)
+        private void ShowFilesInView(string path, int ind)
+        {
+            int n = 1;
+            for (int j = 0; j < listbView.Items.Count; j++)
+            {
+                if (listbView.Items[j].ToString() == path)
+                {
+                    foreach (string dirs in Directory.GetFiles(path))
+                    {
+                        listbView.Items.Insert(j + n, "   " + dirs);
+                    }
+                }
+            }
+        }
+
+        // добавление папок в Items(listbView)
+        private void ShowDirectorysInView(string path, int ind)
+        {
+            int n = 1;
+            for (int j = 0; j < listbView.Items.Count; j++)
+            {
+                if (listbView.Items[j].ToString() == path)
+                {
+                    foreach (string dirs in Directory.GetDirectories(path))
+                    {
+                       listbView.Items.Insert(j + n, "   " + dirs);
+                    }
+                }
+            }
+            //foreach (string dirs in Directory.GetDirectories(path))
+            //{
+            //    if(listbView.Items.ToString() == path)
+            //    {
+            //        listbView.Items.Insert(listbView.Items. n, "  " + dirs);
+            //    }
+        }
+
+
+
+        // вывод в текстБокс listbView
+        private void showItemsListBoxView()
+        {
+            for (int i = 0; i < listbView.Items.Count; i++)
+            {
+                listbView.Text = listbView.Items[i].ToString();
+            }
+        }
+
+        #endregion
+
+
+        private void listbDisk_DoubleClick(object sender, EventArgs e)
+        {
+            bool flagInList = false;
+            if(listbDisk.SelectedItem != null)
+            {
+                for (int i = 0; i < driveInfo.Count; i++)
+                {
+                    if (listbDisk.SelectedItem == driveInfo[i])
+                        flagInList = true;
+                }
+                if(flagInList != true)
+                {
+                    if (AddDiskToListBoxView == false)
+                    {
+                        foreach (string item in ListFolderView)
+                        {
+                            listbView.Items.Add(item);
+                        }
+                        AddDiskToListBoxView = true;
+                    }
+                    ShowDirectorysInView(listbDisk.SelectedItem.ToString(), listbDisk.SelectedIndex);
+                    ShowFilesInView(listbDisk.SelectedItem.ToString(), listbDisk.SelectedIndex);
+                    showItemsListBoxView();
+                }
+            }
         }
     }
 }
