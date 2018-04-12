7d6425225a106ba4a5de92b9f5a02331159846a8
mitGmailCom <shutkovdmitrij@gmail.com>
2018-04-12 10:07:23 +0300

    Create project CreateMenu

---
+++ Project Create Menu/CreateMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Create_Menu
+{
+    public partial class FormCreateMenu : Form
+    {
+        public FormCreateMenu()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnAddTopLevelMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
