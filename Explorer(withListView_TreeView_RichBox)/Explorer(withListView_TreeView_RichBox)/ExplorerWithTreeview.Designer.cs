6a20c77e145c60f7e418abefa27426c9a82e68db
Admin <Admin@X>
2018-04-13 17:39:44 +0300

    Add ImageList

--- Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.Designer.cs
+++ Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.Designer.cs
@@ -28,23 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formExplorerTreeview));
             this.txbPath = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallIconToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.treeViewExpl = new System.Windows.Forms.TreeView();
             this.listViewExpl = new System.Windows.Forms.ListView();
             this.richTxbExpl = new System.Windows.Forms.RichTextBox();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1Large = new System.Windows.Forms.ImageList(this.components);
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txbPath
             // 
-            this.txbPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txbPath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txbPath.Location = new System.Drawing.Point(0, 24);
             this.txbPath.Name = "txbPath";
-            this.txbPath.Size = new System.Drawing.Size(575, 20);
+            this.txbPath.Size = new System.Drawing.Size(718, 20);
             this.txbPath.TabIndex = 0;
             // 
             // menuStrip1
@@ -55,35 +65,9 @@
             this.editToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(575, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(718, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // treeViewExpl
-            // 
-            this.treeViewExpl.Dock = System.Windows.Forms.DockStyle.Left;
-            this.treeViewExpl.Location = new System.Drawing.Point(0, 44);
-            this.treeViewExpl.Name = "treeViewExpl";
-            this.treeViewExpl.Size = new System.Drawing.Size(127, 315);
-            this.treeViewExpl.TabIndex = 2;
-            // 
-            // listViewExpl
-            // 
-            this.listViewExpl.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listViewExpl.Location = new System.Drawing.Point(127, 44);
-            this.listViewExpl.Name = "listViewExpl";
-            this.listViewExpl.Size = new System.Drawing.Size(224, 315);
-            this.listViewExpl.TabIndex = 3;
-            this.listViewExpl.UseCompatibleStateImageBehavior = false;
-            // 
-            // richTxbExpl
-            // 
-            this.richTxbExpl.Dock = System.Windows.Forms.DockStyle.Left;
-            this.richTxbExpl.Location = new System.Drawing.Point(351, 44);
-            this.richTxbExpl.Name = "richTxbExpl";
-            this.richTxbExpl.Size = new System.Drawing.Size(224, 315);
-            this.richTxbExpl.TabIndex = 4;
-            this.richTxbExpl.Text = "";
             // 
             // fileToolStripMenuItem
             // 
@@ -93,9 +77,50 @@
             // 
             // viewToolStripMenuItem
             // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showIconToolStripMenuItem});
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
             this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.viewToolStripMenuItem.Text = "View";
+            // 
+            // showIconToolStripMenuItem
+            // 
+            this.showIconToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.largeIconToolStripMenuItem,
+            this.smallIconToolStripMenuItem1,
+            this.listToolStripMenuItem,
+            this.tableToolStripMenuItem});
+            this.showIconToolStripMenuItem.Name = "showIconToolStripMenuItem";
+            this.showIconToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showIconToolStripMenuItem.Text = "ShowIcon";
+            // 
+            // largeIconToolStripMenuItem
+            // 
+            this.largeIconToolStripMenuItem.Name = "largeIconToolStripMenuItem";
+            this.largeIconToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.largeIconToolStripMenuItem.Text = "Large";
+            this.largeIconToolStripMenuItem.Click += new System.EventHandler(this.largeIconToolStripMenuItem_Click);
+            // 
+            // smallIconToolStripMenuItem1
+            // 
+            this.smallIconToolStripMenuItem1.Name = "smallIconToolStripMenuItem1";
+            this.smallIconToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.smallIconToolStripMenuItem1.Text = "Small";
+            this.smallIconToolStripMenuItem1.Click += new System.EventHandler(this.smallIconToolStripMenuItem1_Click);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // tableToolStripMenuItem
+            // 
+            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
+            this.tableToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tableToolStripMenuItem.Text = "Table";
+            this.tableToolStripMenuItem.Click += new System.EventHandler(this.tableToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
@@ -103,11 +128,50 @@
             this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
             this.editToolStripMenuItem.Text = "Edit";
             // 
+            // treeViewExpl
+            // 
+            this.treeViewExpl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeViewExpl.Location = new System.Drawing.Point(0, 44);
+            this.treeViewExpl.Name = "treeViewExpl";
+            this.treeViewExpl.Size = new System.Drawing.Size(189, 315);
+            this.treeViewExpl.TabIndex = 2;
+            this.treeViewExpl.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewExpl_AfterSelect);
+            // 
+            // listViewExpl
+            // 
+            this.listViewExpl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewExpl.Location = new System.Drawing.Point(189, 44);
+            this.listViewExpl.Name = "listViewExpl";
+            this.listViewExpl.Size = new System.Drawing.Size(250, 315);
+            this.listViewExpl.TabIndex = 3;
+            this.listViewExpl.UseCompatibleStateImageBehavior = false;
+            // 
+            // richTxbExpl
+            // 
+            this.richTxbExpl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTxbExpl.Location = new System.Drawing.Point(439, 44);
+            this.richTxbExpl.Name = "richTxbExpl";
+            this.richTxbExpl.Size = new System.Drawing.Size(279, 315);
+            this.richTxbExpl.TabIndex = 4;
+            this.richTxbExpl.Text = "";
+            // 
+            // imageList1Large
+            // 
+            this.imageList1Large.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1Large.ImageStream")));
+            this.imageList1Large.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1Large.Images.SetKeyName(0, "bltinmic.ico");
+            // 
             // formExplorerTreeview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 359);
+            this.ClientSize = new System.Drawing.Size(718, 359);
             this.Controls.Add(this.richTxbExpl);
             this.Controls.Add(this.listViewExpl);
             this.Controls.Add(this.treeViewExpl);
@@ -134,6 +198,12 @@
         private System.Windows.Forms.TreeView treeViewExpl;
         private System.Windows.Forms.ListView listViewExpl;
         private System.Windows.Forms.RichTextBox richTxbExpl;
+        private System.Windows.Forms.ToolStripMenuItem showIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallIconToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tableToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList1Large;
     }
 }
 
--- Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.cs
+++ Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.cs
@@ -18,6 +18,8 @@ namespace Explorer_withListView_TreeView_RichBox_
     {
         TreeNode root;
         List<TreeNode> listTreeNodes;
+        ImageList largeIcon;
+        ImageList smallIcon;
         public formExplorerTreeview()
         {
             InitializeComponent();
@@ -28,6 +30,13 @@ namespace Explorer_withListView_TreeView_RichBox_
         // zagruzit metod (diski);
         private void formExplorerTreeview_Load(object sender, EventArgs e)
         {
+            largeIcon = new ImageList();
+            largeIcon.ImageSize = new Size(32, 32);
+            largeIcon.ColorDepth = ColorDepth.Depth32Bit;
+            smallIcon = new ImageList();
+            smallIcon.ImageSize = new Size(16, 16);
+            smallIcon.ColorDepth = ColorDepth.Depth32Bit;
+
             root = new TreeNode("My_computer");
             treeViewExpl.Nodes.Add(root);
             listTreeNodes = new List<TreeNode>();
@@ -43,11 +52,11 @@ namespace Explorer_withListView_TreeView_RichBox_
                 TreeNode logicD = new TreeNode();
                 if (item.IsReady)
                 {
-                    logicD.Text = item.Name;/*.TrimEnd('\\');*/
-                    //logicD.Name = item.Name;
+                    logicD.Text = item.Name.TrimEnd('\\');
                     logicD.Remove();
                     root.Nodes.Add(logicD);
-                    LoadDirectory(logicD.Text, logicD, 0);
+
+                    LoadDirectory(logicD.Text + "\\", logicD, 0);
                 }
             }
         }
@@ -55,14 +64,11 @@ namespace Explorer_withListView_TreeView_RichBox_
         // Directory
         private void LoadDirectory(string path, TreeNode parent, int level)
         {
-            //level++;
-            //if (level > 4)
-            //    return;
             foreach (var item in Directory.GetDirectories(path))
             {
-                //level++;
-                //if (level > 50)
-                //    return;
+                level++; // uvelichivaem kol-vo vlogenih elementov
+                if (level > 30)
+                    return;
                 TreeNode subDir = new TreeNode();
                 DirectoryInfo dir = new DirectoryInfo(item);
                 if (dir.GetType().IsVisible)
@@ -71,17 +77,150 @@ namespace Explorer_withListView_TreeView_RichBox_
                     {
                         subDir.Text = dir.Name;
                         parent.Nodes.Add(subDir);
-                        LoadDirectory(dir.FullName, subDir, 0);
+                        LoadDirectory(dir.FullName, subDir, level);
                     }
                     catch
                     {
-                        
                     }
-    
                 }
             }
         }
 
 
+        // Vivod Puti v txbPath
+        private void treeViewExpl_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            bool flagIfDisk = false;
+            listViewExpl.Items.Clear();
+            try
+            {
+                if (treeViewExpl.SelectedNode.FullPath == root.FullPath) // esli vibrali "My_computer"
+                    IfSelectedMyComp(e, flagIfDisk);
+                
+
+                if (treeViewExpl.SelectedNode.FullPath != root.FullPath) // esli vibrali ne "My_computer"
+                    IfNotSelectedMyComp(e, flagIfDisk);
+            }
+            catch { }
+            
+        }
+
+
+
+        private bool IsDisk(TreeViewEventArgs e)
+        {
+            bool tempFlag = false;
+            foreach (var logicDrive in Directory.GetLogicalDrives())
+            {
+                if (logicDrive.TrimEnd('\\') == e.Node.Text)
+                    return tempFlag = true;
+            }
+
+            return tempFlag;
+        }
+
+
+
+        private void IfSelectedMyComp(TreeViewEventArgs e, bool flagIfDisk)
+        {
+            txbPath.Text = e.Node.FullPath.Substring(root.FullPath.Length);
+            flagIfDisk = IsDisk(e);
+            if (flagIfDisk == false)
+            {
+                foreach (var item in Directory.GetDirectories(e.Node.FullPath.Substring(root.FullPath.Length)))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item);
+                    listViewExpl.Items.Add(dir.Name);
+                }
+
+                foreach (var item1 in Directory.GetFiles(e.Node.FullPath.Substring(root.FullPath.Length)))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item1);
+                    listViewExpl.Items.Add(dir.Name);
+                }
+            }
+            else if (flagIfDisk == true)
+            {
+                foreach (var item in Directory.GetDirectories(e.Node.FullPath.Substring(root.FullPath.Length) + "\\"))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item);
+                    listViewExpl.Items.Add(dir.Name);
+                }
+
+                foreach (var item1 in Directory.GetFiles(e.Node.FullPath.Substring(root.FullPath.Length) + "\\"))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item1);
+                    listViewExpl.Items.Add(dir.Name);
+                }
+            }
+        }
+
+
+        private void IfNotSelectedMyComp(TreeViewEventArgs e, bool flagIfDisk)
+        {
+            txbPath.Text = e.Node.FullPath.Substring(root.FullPath.Length + 1);
+
+            flagIfDisk = IsDisk(e);
+
+            if (flagIfDisk == false)
+            {
+                foreach (var item in Directory.GetDirectories(e.Node.FullPath.Substring(root.FullPath.Length + 1)))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item);
+                    largeIcon.Images.Add(dir.Name + "Key", Icon.ExtractAssociatedIcon(dir.FullName));
+                    listViewExpl.Items.Add(dir.Name);
+                }
+
+                foreach (var item1 in Directory.GetFiles(e.Node.FullPath.Substring(root.FullPath.Length + 1)))
+                {
+                    FileInfo file = new FileInfo(item1);
+                    largeIcon.Images.Add(file.Name + "Key", Icon.ExtractAssociatedIcon(file.FullName));
+                    listViewExpl.Items.Add(file.Name);
+                }
+            }
+
+            else if (flagIfDisk == true)
+            {
+                foreach (var item in Directory.GetDirectories(e.Node.FullPath.Substring(root.FullPath.Length + 1) + "\\"))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(item);
+                    //largeIcon.Images.Add(dir.Name + "Key", Icon.ExtractAssociatedIcon(dir.FullName));
+                    listViewExpl.Items.Add(dir.Name);
+                }
+
+                foreach (var item1 in Directory.GetFiles(e.Node.FullPath.Substring(root.FullPath.Length + 1) + "\\"))
+                {
+                    FileInfo file = new FileInfo(item1);
+                    largeIcon.Images.Add(file.Name, (Icon)Icon.ExtractAssociatedIcon(file.FullName));
+                    listViewExpl.Items.Add(file.Name, file.Name);
+                }
+            }
+        }
+
+
+        
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExpl.View = View.List;
+            listViewExpl.SmallImageList = smallIcon;
+        }
+
+        private void tableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void largeIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExpl.View = View.LargeIcon;
+            listViewExpl.LargeImageList = largeIcon;
+        }
+
+        private void smallIconToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            listViewExpl.View = View.SmallIcon;
+            listViewExpl.SmallImageList = smallIcon;
+        }
     }
 }
