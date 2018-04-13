e2f5264066be8c9ea3a8a08563acacd67359e9fa
Admin <Admin@X>
2018-04-13 11:27:12 +0300

    Add LoadDirectory

--- Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.Designer.cs
+++ Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbPath = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.treeViewExpl = new System.Windows.Forms.TreeView();
+            this.listViewExpl = new System.Windows.Forms.ListView();
+            this.richTxbExpl = new System.Windows.Forms.RichTextBox();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txbPath
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.textBox1.Location = new System.Drawing.Point(0, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(575, 20);
-            this.textBox1.TabIndex = 0;
+            this.txbPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txbPath.Location = new System.Drawing.Point(0, 24);
+            this.txbPath.Name = "txbPath";
+            this.txbPath.Size = new System.Drawing.Size(575, 20);
+            this.txbPath.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -59,31 +59,31 @@
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // treeView1
+            // treeViewExpl
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.treeView1.Location = new System.Drawing.Point(0, 44);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(127, 315);
-            this.treeView1.TabIndex = 2;
+            this.treeViewExpl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeViewExpl.Location = new System.Drawing.Point(0, 44);
+            this.treeViewExpl.Name = "treeViewExpl";
+            this.treeViewExpl.Size = new System.Drawing.Size(127, 315);
+            this.treeViewExpl.TabIndex = 2;
             // 
-            // listView1
+            // listViewExpl
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listView1.Location = new System.Drawing.Point(127, 44);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(224, 315);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listViewExpl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listViewExpl.Location = new System.Drawing.Point(127, 44);
+            this.listViewExpl.Name = "listViewExpl";
+            this.listViewExpl.Size = new System.Drawing.Size(224, 315);
+            this.listViewExpl.TabIndex = 3;
+            this.listViewExpl.UseCompatibleStateImageBehavior = false;
             // 
-            // richTextBox1
+            // richTxbExpl
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.richTextBox1.Location = new System.Drawing.Point(351, 44);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(224, 315);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
+            this.richTxbExpl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTxbExpl.Location = new System.Drawing.Point(351, 44);
+            this.richTxbExpl.Name = "richTxbExpl";
+            this.richTxbExpl.Size = new System.Drawing.Size(224, 315);
+            this.richTxbExpl.TabIndex = 4;
+            this.richTxbExpl.Text = "";
             // 
             // fileToolStripMenuItem
             // 
@@ -108,14 +108,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 359);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTxbExpl);
+            this.Controls.Add(this.listViewExpl);
+            this.Controls.Add(this.treeViewExpl);
+            this.Controls.Add(this.txbPath);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "formExplorerTreeview";
             this.Text = "Explorer";
+            this.Load += new System.EventHandler(this.formExplorerTreeview_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -125,14 +126,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbPath;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TreeView treeViewExpl;
+        private System.Windows.Forms.ListView listViewExpl;
+        private System.Windows.Forms.RichTextBox richTxbExpl;
     }
 }
 
--- Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.cs
+++ Explorer(withListView_TreeView_RichBox)/Explorer(withListView_TreeView_RichBox)/ExplorerWithTreeview.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
+using System.Security.AccessControl;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,72 @@ namespace Explorer_withListView_TreeView_RichBox_
 {
     public partial class formExplorerTreeview : Form
     {
+        TreeNode root;
+        List<TreeNode> listTreeNodes;
         public formExplorerTreeview()
         {
             InitializeComponent();
         }
+
+        // sozdat "myComp"
+        // zagruzit "myComp"
+        // zagruzit metod (diski);
+        private void formExplorerTreeview_Load(object sender, EventArgs e)
+        {
+            root = new TreeNode("My_computer");
+            treeViewExpl.Nodes.Add(root);
+            listTreeNodes = new List<TreeNode>();
+            LoadDisk();
+        }
+
+        // Diski
+        private void LoadDisk()
+        {
+            
+            foreach (var item in DriveInfo.GetDrives())
+            {
+                TreeNode logicD = new TreeNode();
+                if (item.IsReady)
+                {
+                    logicD.Text = item.Name;/*.TrimEnd('\\');*/
+                    //logicD.Name = item.Name;
+                    logicD.Remove();
+                    root.Nodes.Add(logicD);
+                    LoadDirectory(logicD.Text, logicD, 0);
+                }
+            }
+        }
+
+        // Directory
+        private void LoadDirectory(string path, TreeNode parent, int level)
+        {
+            //level++;
+            //if (level > 4)
+            //    return;
+            foreach (var item in Directory.GetDirectories(path))
+            {
+                //level++;
+                //if (level > 50)
+                //    return;
+                TreeNode subDir = new TreeNode();
+                DirectoryInfo dir = new DirectoryInfo(item);
+                if (dir.GetType().IsVisible)
+                {
+                   try
+                    {
+                        subDir.Text = dir.Name;
+                        parent.Nodes.Add(subDir);
+                        LoadDirectory(dir.FullName, subDir, 0);
+                    }
+                    catch
+                    {
+                        
+                    }
+    
+                }
+            }
+        }
+
+
     }
 }
